0f6f265b7a567b4be169859b08d8fb0c0bbdce45
Matthieu LAURENT <matthieu.laurent69@proton.me>
2023-04-17 17:32:52 +0200

    Added a CLI, and disconnection handling

--- NumworksFS/numworks_fs.py
+++ NumworksFS/numworks_fs.py
@@ -1,21 +1,41 @@
+from __future__ import annotations
+
 import asyncio
 import logging
 import os
 from errno import EIO, ENOENT
+from functools import wraps
 from io import BytesIO
 from pathlib import Path
 from stat import S_IFDIR, S_IFREG
+from typing import Callable, NoReturn
 
 import upsilon_py
-from fuse import FuseOSError, LoggingMixIn, Operations
+from fuse import FuseOSError, LoggingMixIn, Operations, fuse_exit
 
 from NumworksFS.storage_handler import NumworkFile, NumworksStorage
 
 logger = logging.getLogger(__name__)
 
 
+def needs_connection(func: Callable):
+    @wraps(func)
+    def wrapper(self: "NumworksFS", *args, **kwargs):
+        # If we're not connected anymore
+        if (
+            self.loop.run_until_complete(self.numworks.status())["status"]
+            != "connected"
+        ):
+            # We don't reconnect, the script exits
+            self._exit()
+
+        return func(self, *args, **kwargs)
+
+    return wrapper
+
+
 class NumworksFS(Operations, LoggingMixIn):
-    """A read-only filesystem to access numworks files"""
+    """A read-write filesystem to access numworks files"""
 
     async def _setup_numworks(self):
         logger.info("Setting up numworks connection.")
@@ -24,7 +44,12 @@ class NumworksFS(Operations, LoggingMixIn):
         await self.numworks.connect()
         logger.info("Numworks connection ready!")
 
-    def __init__(self):
+    def _exit(self) -> NoReturn:
+        fuse_exit()
+        self.loop.run_until_complete(self.numworks.stop())
+        exit(0)
+
+    def __init__(self) -> None:
         self.root = "/"
         self.loop = asyncio.new_event_loop()
         self.loop.run_until_complete(self._setup_numworks())
@@ -37,6 +62,7 @@ class NumworksFS(Operations, LoggingMixIn):
         if not path.suffix == ".py":
             raise FuseOSError(EIO)
 
+    @needs_connection
     def readdir(self, path, fh):
         logger.info(f"Reading dir {path}")
         p = Path(path)
@@ -47,6 +73,7 @@ class NumworksFS(Operations, LoggingMixIn):
             file_names = [str(f.filename) for f in s.files]
         return [".", ".."] + file_names
 
+    @needs_connection
     def read(self, path, size, offset, fh):
         logger.info(f"Reading file {path}. {size=}, {offset=}")
         with NumworksStorage(self.numworks, self.loop) as s:
@@ -55,6 +82,7 @@ class NumworksFS(Operations, LoggingMixIn):
         f.seek(offset)
         return f.read(size)
 
+    @needs_connection
     def getattr(self, path, fh=None):
         logger.info(f"Getting attr for {path}")
 
@@ -83,6 +111,7 @@ class NumworksFS(Operations, LoggingMixIn):
     # It's needed for some reason
     getxattr = None
 
+    @needs_connection
     def create(self, path, mode=0o777):
         logger.info(f"Creating file {path}. {mode=}")
         p = Path(path)
@@ -96,6 +125,7 @@ class NumworksFS(Operations, LoggingMixIn):
     #     logger.info(f"Attempted mkdir with {path = }")
     #     raise FuseOSError(EIO)
 
+    @needs_connection
     def write(self, path, data, offset, fh):
         logger.info(f"Writting to file {path}.")
         p = Path(path)
@@ -116,6 +146,7 @@ class NumworksFS(Operations, LoggingMixIn):
 
         return len(data)
 
+    @needs_connection
     def unlink(self, path):
         logger.info(f"Unlinking file {path}.")
 
@@ -126,6 +157,7 @@ class NumworksFS(Operations, LoggingMixIn):
                 raise FuseOSError(ENOENT)
             s.files.remove(file)
 
+    @needs_connection
     def rename(self, old_path, new_path) -> None:
         logger.info(f"Renaming file: {old_path} -> {new_path}")
         old_p = Path(old_path)
@@ -138,11 +170,12 @@ class NumworksFS(Operations, LoggingMixIn):
                 raise FuseOSError(ENOENT)
             file.name = new_p.name.removesuffix(".py")
 
+    @needs_connection
     def statfs(self, path):
         logger.info(f"Statfs for path {path}")
 
         if path != self.root:
-            return None
+            raise FuseOSError(EIO)
 
         info = self.loop.run_until_complete(self.numworks.get_platform_info())
         with NumworksStorage(self.numworks, self.loop) as s:
@@ -159,12 +192,14 @@ class NumworksFS(Operations, LoggingMixIn):
             "f_namemax": 219,
         }
 
+    @needs_connection
     def ioctl(self, path, cmd, arg, fh, flags, data):
         logger.info(f"IOctl: {path=}, {cmd=}, {arg=}, {fh=}, {flags=}, {data=}")
         # * Required for nano and micro to work
         if cmd == 0x5401:
             return True
 
+    @needs_connection
     def truncate(self, path, length, fh=None):
         logger.info(f"Truncating file {path}. {length=}")
         p = Path(path)
--- run.py
+++ run.py
@@ -1,23 +1,65 @@
 from fuse import FUSE
 
 import logging
+import argparse
 import sys
 
 from NumworksFS import NumworksFS
 
 
-def main(mountpoint: str):
+def main(mountpoint: str, *, foreground: bool):
     FUSE(
         operations=NumworksFS(),
         mountpoint=mountpoint,
         encoding="utf-8",
         nothreads=True,
-        foreground=True,
+        foreground=foreground,
         allow_other=True,
     )
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    parser = argparse.ArgumentParser(
+        description="A FUSE to access your NumWorks calculator's files"
+    )
+    parser.add_argument("mount", help="directory to mount to", type=str)
+    parser.add_argument(
+        "-v", "--verbose", help="increase output verbosity", action="store_true"
+    )
 
-    main(sys.argv[1])
+    mode = parser.add_mutually_exclusive_group()
+    mode.add_argument(
+        "-f",
+        "--foreground",
+        action="store_true",
+        help="run in foreground mode",
+    )
+    mode.add_argument(
+        "-b",
+        "--background",
+        action="store_true",
+        help="run in background mode",
+        default=True,
+    )
+
+    # reconnect_group = parser.add_mutually_exclusive_group(required=False)
+    # reconnect_group.add_argument(
+    #     "-r",
+    #     "--reconnect",
+    #     dest="reconnect",
+    #     action="store_true",
+    #     help="waits for the calculator to be plugged back in when connection is lost",
+    # )
+    # reconnect_group.add_argument(
+    #     "--no-reconnect",
+    #     dest="reconnect",
+    #     action="store_false",
+    #     help="exits as soon as the connection to the calculator is lost",
+    # )
+    # parser.set_defaults(reconnect=True)
+    args = parser.parse_args()
+
+    level = logging.DEBUG if args.verbose else logging.WARNING
+    logging.basicConfig(level=level)
+
+    main(args.mount, foreground=args.foreground)
